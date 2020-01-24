46759f8b02dce1a9790a80693b2fd4df6e4351ea
enrach21 <ijones@berkeley.edu>
2020-01-23 20:28:43 -0800

    got rid of complexity function

--- example/algs.py
+++ example/algs.py
@@ -4,17 +4,26 @@ import math
 def insertion_sort(x):
     """
     Goals: 
+    The goal is to sort the an array by starting with the left most element
+    and moving left to right move an element back if it is less than the previous
+    element until the list is completely ordered.
     
     Variables:
     x is the array inputed into the function.
-    Assign counts when ever an index is moved or when two elements are switched
-    Condition counts everytime two elements are compared
-    n is the lengh of the array
+    Assign counts when ever an index is moved or when two elements are switched.
+    Condition counts everytime two elements are compared.
+    n is the lengh of the array.
+    i is the pointer to which element of the array is being looked at.
     
+    Output:
+    Array of...
+    first is sorted array
+    second is amount of conditionals called
+    thris is the number of assignments
     """
     Assign = 0
     conditional = 0
-    for i in range (1, len(x)):
+    for i in range (1, len(x)): # pass through the array skipping the 0 element
         Assign +=1
         conditional +=1
         while x[i] < x[i-1]:
@@ -25,23 +34,29 @@ def insertion_sort(x):
             conditional +=1
             if i == 0:
                 break
-    return x, Assign, conditional, Assign
+    return x, Assign, conditional
 
 
 def bubbleSort(x):
     """
     Goals:
+    The goal is to sort the array by looking at two element at a time and
+    switching them if the first is less than the second and move up the array.
+    Eventually after traverseing N times through the list everything will be completley
+    sorted.
     
     Variables:
     x is the array inputed into the function.
     Assign counts when ever an index is moved or when two elements are switched
     Condition counts everytime two elements are compared
     n is the lengh of the array
-   
-   
     
-    Good Commands:
-    len(), will provide length of an array
+    Output:
+    Array of...
+    first is sorted array
+    second is amount of conditionals called
+    thris is the number of assignments
+   
     """
     Assign = 0
     Condition = 0
@@ -66,6 +81,10 @@ def bubbleSort(x):
 def bubbleSortRecursive(x, Complexity):
     """
     Goals:
+    The goal is to sort the array by looking at two element at a time and
+    switching them if the first is less than the second and move up the array.
+    Rather than going through the array N times however, there is a parameter
+    checking to see if 
     
     Variables:
     recursive is a varaible used to determine if the if statement
@@ -77,18 +96,20 @@ def bubbleSortRecursive(x, Complexity):
     Condition counts everytime two elements are compared.
     n is the lengh of the array.
     
-    Good Commands:
-    len(), will provide length of an array
+    Output:
     """
-    C = 0
+    Assign = 0
+    Condition = 0
     recursive=0
     for i in range (1, len(x)):
-        C += 1
+        Assign += 1
         print('i is equal to ' + str(i))
         j = i-1
+        Assign += 1
         print('j is equal to ' + str(j))
         num_1 = x[j]
         num_2 = x[i]
+        Condition += 1
         if num_1 > num_2:
             temp = num_1
             x[j] = x[i]
@@ -183,53 +204,6 @@ def quicksort_Assign(x):
                 Assign +=1
         return quicksort_Assign(Array1) + Assign + quicksort_Assign(Array2)
         
-        
-def complexity():
-    """
-    Describe how you are sorting `x`
-    
-    Goal: 
-    First: Select a pivot point. In this case the first bin in the list.
-    Second: Put all of other items in the list ordered arround the pivot variable
-    Third: Recursively do the same to each side of the list
-    Fourth: If the list being sorted is 0 return it
-    
-    
-    Variables:
-    P is the pivot point
-    
-    """
-    X = np.arange(100,1100,100)
-    
-    # N complexity
-    N_Y = np.array([])
-    for i in X:
-        Array = np.random.rand(i)
-        N_Y = np.append(N_Y, i)
-    
-    # Nlog(N) complexity 
-    N_logN_Y = np.array([])
-    for i in X:
-        Array = np.random.rand(i)
-        N_logN_Y = np.append(N_logN_Y, (i*log(i)))
-    
-    # N^2 complexity                         
-    N2Y = np.array([])
-    for i in X:
-        Array = np.random.rand(i)
-        N2Y = np.append(N2Y, (i*i))
-    
-    Insertion_Y = np.array([])
-    for i in X:
-        Array = np.random.rand(i)
-        Insertion_Y = np.append(Insertion_Y, insertion_sort_complexity(Array))
-
-    
-    plt.plot(X, N_Y, 'o', color='blue', label='O(n)')
-    plt.plot(X, N2Y, 'o', color='green', label='O(n^2)')
-    plt.plot(X, Insertion_Y, 'o', color='black', label='Insertion Sort')
-    plt.show()
-    return
 
 X = np.arange(100,1100,100)
     
@@ -251,17 +225,17 @@ for i in X:
 Insertion_Y = np.array([])
 for i in X:
     Array = np.random.rand(i)
-    Insertion_Y = np.append(Insertion_Y, insertion_sort(Array, Complexity=True))
+    Insertion_Y = np.append(Insertion_Y, insertion_sort(Array)[0])
     
 Bubble_Y = np.array([])
 for i in X:
     Array = np.random.rand(i)
-    Bubble_Y = np.append(Bubble_Y,  bubbleSort(Array, Complexity=True))
+    Bubble_Y = np.append(Bubble_Y,  bubbleSort(Array)[0])
     
 Quick_Y = np.array([])
 for i in X:
     Array = np.random.rand(i)
-    Quick_Y = np.append(Quick_Y,  quicksort(Array, Complexity=True))
+    Quick_Y = np.append(Quick_Y,  quicksort_Assign(Array))
 
 
 plt.plot(X, N_Y, 'o', color='blue', label='O(n)')
